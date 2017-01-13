d8da763052887ccffb45983772aca3cb452eefcd
Nicolas Morel <nicolas.morel@millevolts.fr>
2017-01-13 21:25:01 +0100

    Mise à jour des libs

--- android/app/src/main/java/com/magiccounter/MainActivity.java
+++ android/app/src/main/java/com/magiccounter/MainActivity.java
@@ -1,14 +1,9 @@
 package com.magiccounter;
 
-import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-
 import android.os.Bundle;
 import android.view.WindowManager;
 
-import java.util.Arrays;
-import java.util.List;
+import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
 
@@ -21,30 +16,9 @@ public class MainActivity extends ReactActivity {
         return "MagicCounter";
     }
 
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
     @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    /**
-     * A list of packages used by the app. If the app uses additional views
-     * or modules besides the default ones, add more packages here.
-     */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
-        );
-    }
-
-    @Override
-    protected void onCreate(Bundle icicle)
-    {
-        super.onCreate(icicle);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 }
